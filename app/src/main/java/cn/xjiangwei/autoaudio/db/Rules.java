7b3543534c721ca44a36fb8fb874aaa8a34cc5dc
jiangwei <jiangwei@mafengwo.com>
2018-12-28 19:02:11 +0800

    init

--- app/src/main/java/cn/xjiangwei/autoaudio/db/Rules.java
+++ app/src/main/java/cn/xjiangwei/autoaudio/db/Rules.java
@@ -1,11 +1,15 @@
 package cn.xjiangwei.autoaudio.db;
 
+import org.litepal.LitePal;
 import org.litepal.annotation.Column;
 import org.litepal.crud.LitePalSupport;
 
+import java.util.List;
+
 public class Rules extends LitePalSupport {
 
-
+    private static final int OPEN = 2;
+    private static final int CLOSE = 1;
     private int id;
 
     @Column(nullable = true)
@@ -18,8 +22,9 @@ public class Rules extends LitePalSupport {
     private int hour;
     @Column(nullable = true)
     private int min;
-    private int status;
-
+    private int audio;
+    private int ring;
+    private int clock;
 
     public int getYear() {
         return year;
@@ -61,20 +66,84 @@ public class Rules extends LitePalSupport {
         this.min = min;
     }
 
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+
+    public int getAudio() {
+        return audio;
+    }
+
+    public void setAudio(int audio) {
+        this.audio = audio;
+    }
+
+    public int getRing() {
+        return ring;
+    }
+
+    public void setRing(int ring) {
+        this.ring = ring;
+    }
+
+    public int getClock() {
+        return clock;
+    }
+
+    public void setClock(int clock) {
+        this.clock = clock;
+    }
+
+
+    public static boolean addRules(int year, int month, int day, int hour, int min, int audio, int ring, int clock) {
+        Rules rules = new Rules();
+        if (year > 0) rules.setYear(year);
+        if (month > 0) rules.setMonth(month);
+        if (day > 0) rules.setDay(day);
+        if (hour > 0) rules.setHour(hour);
+        if (min > 0) rules.setMin(min);
+        if (audio > 0) rules.setAudio(audio);
+        if (ring > 0) rules.setRing(audio);
+        if (clock > 0) rules.setClock(clock);
+        return rules.save();
+    }
+
+
+    public static int[] checkStatus(int year, int month, int day, int hour, int min) {
+        int ring = 0;
+        int audio = 0;
+        int clock = 0;
+
+        List<Rules> rulesList = LitePal
+                .where("year = ? or year =0 ", String.valueOf(year))
+                .where("month = ? or month = 0", String.valueOf(month))
+                .where("day = ? or day = 0", String.valueOf(day))
+                .where("hour = ? or hour = 0", String.valueOf(hour))
+                .where("min = ? or min = 0", String.valueOf(min))
+                .find(Rules.class);
+        for (Rules rule : rulesList) {
+            if (rule.getAudio() == Rules.OPEN && audio == 0) {
+                audio = Rules.OPEN;
+            } else if (rule.getAudio() == Rules.CLOSE && audio == 0) {
+                audio = Rules.CLOSE;
+            }
+            if (rule.getRing() == Rules.OPEN && ring == 0) {
+                ring = Rules.OPEN;
+            } else if (rule.getRing() == Rules.CLOSE && ring == 0) {
+                ring = Rules.CLOSE;
+            }
+            if (rule.getClock() == Rules.OPEN && clock == 0) {
+                clock = Rules.OPEN;
+            } else if (rule.getClock() == Rules.CLOSE && clock == 0) {
+                clock = Rules.CLOSE;
+            }
+        }
+        return new int[]{audio,ring,clock};
     }
 
 
@@ -87,7 +156,9 @@ public class Rules extends LitePalSupport {
                 ", day=" + day +
                 ", hour=" + hour +
                 ", min=" + min +
-                ", status=" + status +
+                ", audio=" + audio +
+                ", ring=" + ring +
+                ", clock=" + clock +
                 '}';
     }
 }
